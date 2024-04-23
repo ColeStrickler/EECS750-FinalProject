e3fb75b25eb0e11fb1dd91e514c0428ec0dba567
Cole Strickler <colestrickler@gmail.com>
2024-04-23 10:23:06 -0500

    testing

--- test.sh
+++ test.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Check if script is run as root
+if [ "$EUID" -ne 0 ]
+then
+  echo "This script must be run as root" 
+  exit 1
+fi
+
+
+ulimit -n 1000000
 make clean
 make driver
 make remove
 make install
 
 for ((i =  100; i < 1000000; i += 100)); do
-    ./io_driver_com $i 0
+    ./io_driver_com 100 0
 done
